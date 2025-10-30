3e0c3f21674e3c3bcedef40640f93d2e24ca0ced
Siff <hasifsalika1@gmail.com>
2025-10-30 19:32:59 +0700

    Initial Commit

---
+++ Final_CampusConnectApp/Final_CampusConnectAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_CampusConnectAppApp.swift
+//  Final_CampusConnectApp
+//
+//  Created by Hasif Salika on 30/10/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct Final_CampusConnectAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Final_CampusConnectAppTests/Final_CampusConnectAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Final_CampusConnectAppTests.swift
+//  Final_CampusConnectAppTests
+//
+//  Created by Hasif Salika on 30/10/2568 BE.
+//
+
+import Testing
+@testable import Final_CampusConnectApp
+
+struct Final_CampusConnectAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
